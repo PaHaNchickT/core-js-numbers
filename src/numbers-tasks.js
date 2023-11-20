f0cf17cc12229dc9014a1ac30ce4535304686fcb
Pavel <you@example.com>
2023-11-20 20:46:44 +0300

    feat: implement task4 (20.11.23 20:46)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -74,8 +74,8 @@ function getAverage(val1, val2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2);
 }
 
 /**
